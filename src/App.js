e52ac64ab004ec814ed7a6ba4041f5ce343699be
thiagogabrielm <thiagogabrielm@gmail.com>
2021-06-14 17:37:17 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import './App.css';
 import PlotReactVis from './components/PlotReactVis'
+import React, { useState } from 'react';
 
 function App() {
+  const [ferryOne, setFerryOne] = useState(true)
+
+  const aggregation = "snap_mean"
+  const plant21 = "Geirangerfjord2/13185-1"
+  const plant22 = "Geirangerfjord2/13185-2"
+  const plant11 = "Geirangerfjord1/9648-1"
+  const plant12 = "Geirangerfjord1/9648-2"
+  const sensors_geiranger2 = ["rpm", "boost_pressure", "engine_power", "fuel_rate", "cooling_water_temperature", "exhaust_temperature_1", "lube_oil_temperature", "lube_oil_pressure"]
+
+  // const sensors_geiranger2 = ["rpm", "fuel_rate", "cooling_water_temperature", "exhaust_temperature_1", "throttle", "service_battery", "start_battery", "manifold_air_temperature", "exhaust_temperature_2",
+  // "lube_oil_temperature", "lube_oil_pressure", "boost_pressure", "gear_oil_temp", "gear_oil_pressure", "engine_power"]
+
+  const sensors_geiranger1 = ["rpm", "boost_pressure", "engine_power", "fuel_rate", "cooling_water_temperature", "exhaust_temperature_1", "lube_oil_temperature", "lube_oil_pressure"]
+
+  // const sensors_geiranger1 = ["rpm", "fuel_rate", "cooling_water_temperature", "exhaust_temperature_1", "service_battery", "start_battery", "exhaust_temperature_2",
+  // "lube_oil_temperature", "lube_oil_pressure", "boost_pressure", "gear_oil_temp", "gear_oil_pressure", "engine_power"]
+
   return (
+
     <div className="App">
-      <PlotReactVis plant={'ice_water_pump_2'} sensor={'vibration_sensor_1'} aggregation={'mae'}></PlotReactVis>
+      {/* <button onClick={()=>setFerryOne(!ferryOne)}>Change Ferry</button>
+      {console.log(ferryOne)} */}
+      {/* {ferryOne ? ( */}
+      <React.Fragment>
+        {sensors_geiranger1.map((sensor, index) => (
+          <PlotReactVis key={index} plant={plant11} sensor={sensor} aggregation={aggregation}></PlotReactVis>
+        ))}
 
-      <PlotReactVis plant={'ice_water_pump_2'} sensor={'vibration_sensor_2'} aggregation={'mae'}></PlotReactVis>
+        {sensors_geiranger2.map((sensor, index) => (
+          <PlotReactVis key={index} plant={plant22} sensor={sensor} aggregation={aggregation}></PlotReactVis>
+        ))}
+      </React.Fragment>
+      {/* ) : (
+        { console.log("...") }
+      )} */}
+      {/* <PlotReactVis plant={plant} sensor={"gear_oil_temp"} aggregation={aggregation}></PlotReactVis>
 
-      {/* <PlotReactVis plant={'sea_water_pump_2'} sensor={'vibration_sensor_1'} aggregation={'mae'}></PlotReactVis>
+      <PlotReactVis plant={plant} sensor={"service_battery"} aggregation={aggregation}></PlotReactVis> */}
 
-      <PlotReactVis plant={'sea_water_pump_2'} sensor={'vibration_sensor_2'} aggregation={'mae'}></PlotReactVis> */}
-      
-           
+
     </div>
   );
 }
--- src/components/PlotReactVis.js
+++ src/components/PlotReactVis.js
@@ -23,28 +23,31 @@ export default function PlotReactVis({ plant, sensor, aggregation }) {
     ])
 
     const [selection, setSelection] = useState(null)
-    const limit = 1440;
+    const limit = 720;
     const [lastDrawLocation, setLastDrawLocation] = useState(null)
+    const [unit, setUnit] = useState(null)
 
-
+    
     useEffect(() => {
         const interval = setInterval(() => {
 
             const apiCall = async () => {
                 var data_db = [];
                 const options = {
-                    headers: { 'authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ims3NHM4RXR3SEp6RlV4Y19sSDNsUiJ9.eyJpc3MiOiJodHRwczovL2Rldi1kaXBhaS5ldS5hdXRoMC5jb20vIiwic3ViIjoiUjVaNVowaXdqdm1qeTF5MGxMRXlmRmVPR1JjV2dXeXBAY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vYXBpLnRhZmpvcmQubm8iLCJpYXQiOjE2MjM2MDA5MDcsImV4cCI6MTYyMzY4NzMwNywiYXpwIjoiUjVaNVowaXdqdm1qeTF5MGxMRXlmRmVPR1JjV2dXeXAiLCJzY29wZSI6InJlYWQ6c2Vuc29yZGF0YSByZWFkOm1sZGF0YSB3cml0ZTpzZW5zb3JkYXRhIHdyaXRlOm1sZGF0YSIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.IoBgP4TRYAfpQ_h8WqXDxyMZcCL51zhTUZHvBetY1W8pwhGgtok-SMDLUtQxBPRS_Z0gxTeQ-11pNCS6IMOrKaSvz_qkzKXq19PHu6TG9oQ95khIFSQLjUVaIz_6IHG7sTjCrIkUlRjv98XFVRT67HAnnHPV6lFKSOL-t4tXAMYT4J0KQXd0IKl3G-bbsOTp5GW8eFKTsiXCpfHvOlryoGU9wPAJZ7R3RI2fVlbbRrdItjXlHCwiy8y7m6Vh_6Clf2Z_6g4iRm7bHVgK2Leocoti4qd_uqf9MW8AfEWtX_V_8AnO893fm8TWl_3jgJzYTH_emUP34aLyKIth4ZtCBA' }
+                    headers: { 'authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ims3NHM4RXR3SEp6RlV4Y19sSDNsUiJ9.eyJpc3MiOiJodHRwczovL2Rldi1kaXBhaS5ldS5hdXRoMC5jb20vIiwic3ViIjoiZmxJZFhjdzVZSDdmR3E5NzRrU3ZvcHg1Q3RHMWNmRG9AY2xpZW50cyIsImF1ZCI6Imh0dHBzOi8vYXBpLm5vZ3ZhLm5vIiwiaWF0IjoxNjIzNjc2NTgxLCJleHAiOjE2MjM3NjI5ODEsImF6cCI6ImZsSWRYY3c1WUg3ZkdxOTc0a1N2b3B4NUN0RzFjZkRvIiwic2NvcGUiOiJyZWFkOnNlbnNvcmRhdGEgcmVhZDptbGRhdGEgd3JpdGU6c2Vuc29yZGF0YSB3cml0ZTptbGRhdGEiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.kFh2HTDSfhcKL4QDkBg3G4BC4GUZXMLSpbOfHkXYYKVj3v8nim6CxbZINYLynE9kLSfF22eoKTwK8wF0u8UnIj_rF3r4rMJmp0FNNP9-ajmAGMTJZSYD4Zi3y4uImI6JpGzAnISdvmytjdirya1CllrWTM7kmLqoH4NCRUB4R4pDksuDPe_81Ivfy6xq9tbbh4PvFKstc7Jhgu9BpW6z6OVi8LTvY-CEQICkXwGpVXYl7cSo1zmREGSd6y_WnmFSxYFZ1DOAK9Eac0JcY447IJY1ytuzxXeVdGlsBv-F4e1in9zI11Qh7DO7clq1VPpW-MymQfuIVT4DVSVSherMhQ' }
                 };
-                const url = 'https://tafjordhubrestapi.azurewebsites.net/api/sensors?limit=' + limit + '&sensor=' + sensor + '&plant=' + plant + '&aggregation=' + aggregation;
+                const url = 'https://nogvahubrestapi.azurewebsites.net/api/sensors?limit=' + limit + '&sensor=' + sensor + '&plant=' + plant + '&aggregation=' + aggregation;
                 const res = await axios.get(url, options);
                 // console.log(res.data.dbdata[0])
                 // var unit = unitRef.current
                 // unit = res.data.dbdata[0].unit;
-                await data_db.push(res.data.dbdata.map(row => ({ x: new Date(Date.parse(row.time)).getTime(), y: 20 * parseFloat(row.value) })))
+                await data_db.push(res.data.dbdata.map(row => ({ x: new Date(Date.parse(row.time)).getTime(), y: parseFloat(row.value) })))
                 var seriesCopy = series.slice();
                 seriesCopy[0].data = data_db[0]
                 seriesCopy[0].loaded = true
                 setSeries(seriesCopy)
+                setUnit(res.data.dbdata[0].unit)
+                //console.log(res.data.dbdata[0].unit)
                 // console.log(data_db)
                 console.log('apicall')
 
@@ -102,7 +105,7 @@ export default function PlotReactVis({ plant, sensor, aggregation }) {
                             <Borders style={{
                                 all: { fill: 'white' },
                             }} />
-                            <YAxis position='end' title='g' />
+                            <YAxis position='end' title={unit} />
                             <XAxis position='end' title='time' />
 
                             <Highlight
